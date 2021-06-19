2ae42843b0b1e2c48ceb6ce9f79b8146cb468c74
espinosacamilop1 <63834682+espinosacamilop1@users.noreply.github.com>
2021-06-19 15:42:17 -0400

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,13 +1,37 @@
 function sum(a, b) {
-  return;
+ 
+ if(!a && !b){
+    return 0;
+  }
+  if(!a && b){
+    return b + 0;
+  }
+  if(a && !b){
+    return a + 0;
+  }
+return a + b;   
 }
 
+
 function subtract(a, b) {
-  return;
+  
+  if(!a && !b){
+    return 0;
+  }
+  if(!a && b){
+    return b + 0;
+  }
+  if(a && !b){
+    return a + 0;
+  }
+  return a - b;
 }
 
 function divide(a, b) {
-  return;
+  if(b === 0){
+    throw Error(`Cant be divided by 0!!!`)
+  }
+  return a / b;
 }
 
 function multiply(a, b) {
@@ -16,3 +40,4 @@ function multiply(a, b) {
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
+
